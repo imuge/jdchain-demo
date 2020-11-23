cde13f79ec2309ebce07d215a15d6fcd6584d127
imuge <liuyuanmu@jd.com>
2020-11-23 14:33:49 +0800

    add more contract samples

--- contract-compile/src/main/java/com/jd/chain/contract/SetKVContract.java
+++ contract-compile/src/main/java/com/jd/chain/contract/SetKVContract.java
@@ -2,11 +2,53 @@ package com.jd.chain.contract;
 
 import com.jd.blockchain.contract.Contract;
 import com.jd.blockchain.contract.ContractEvent;
-import com.jd.blockchain.ledger.BlockchainIdentity;
 
 @Contract
 public interface SetKVContract {
 
+    /**
+     * 设置KV
+     *
+     * @param address 数据账户地址
+     * @param key     键
+     * @param value   值
+     * @param version 版本
+     */
     @ContractEvent(name = "setkv")
     void setKV(String address, String key, String value, long version);
+
+    /**
+     * 注册用户
+     *
+     * @param seed 种子，不小于32个字符
+     */
+    @ContractEvent(name = "registerUser")
+    void registerUser(String seed);
+
+    /**
+     * 注册数据账户
+     *
+     * @param seed 种子，不小于32个字符
+     */
+    @ContractEvent(name = "registerDataAccount")
+    void registerDataAccount(String seed);
+
+    /**
+     * 注册事件账户
+     *
+     * @param seed 种子，不小于32个字符
+     */
+    @ContractEvent(name = "registerEventAccount")
+    void registerEventAccount(String seed);
+
+    /**
+     * 发布事件
+     *
+     * @param address  事件账户地址
+     * @param topic    消息名称
+     * @param content  内容
+     * @param sequence 当前消息名称下最大序号（初始为-1）
+     */
+    @ContractEvent(name = "publishEvent")
+    void publishEvent(String address, String topic, String content, long sequence);
 }
--- contract-compile/src/main/java/com/jd/chain/contract/SetKVContractImpl.java
+++ contract-compile/src/main/java/com/jd/chain/contract/SetKVContractImpl.java
@@ -2,6 +2,11 @@ package com.jd.chain.contract;
 
 import com.jd.blockchain.contract.ContractEventContext;
 import com.jd.blockchain.contract.EventProcessingAware;
+import com.jd.blockchain.crypto.AsymmetricKeypair;
+import com.jd.blockchain.crypto.Crypto;
+import com.jd.blockchain.crypto.CryptoAlgorithm;
+import com.jd.blockchain.crypto.SignatureFunction;
+import com.jd.blockchain.ledger.BlockchainKeypair;
 import com.jd.blockchain.utils.Bytes;
 
 public class SetKVContractImpl implements EventProcessingAware, SetKVContract {
@@ -10,11 +15,39 @@ public class SetKVContractImpl implements EventProcessingAware, SetKVContract {
 
     @Override
     public void setKV(String address, String key, String value, long version) {
-        try {
-            eventContext.getLedger().dataAccount(Bytes.fromBase58(address)).setText(key, value, version);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        eventContext.getLedger().dataAccount(Bytes.fromBase58(address)).setText(key, value, version);
+    }
+
+    @Override
+    public void registerUser(String seed) {
+        CryptoAlgorithm algorithm = Crypto.getAlgorithm("ed25519");
+        SignatureFunction signFunc = Crypto.getSignatureFunction(algorithm);
+        AsymmetricKeypair cryptoKeyPair = signFunc.generateKeypair(seed.getBytes());
+        BlockchainKeypair keypair = new BlockchainKeypair(cryptoKeyPair.getPubKey(), cryptoKeyPair.getPrivKey());
+        eventContext.getLedger().users().register(keypair.getIdentity());
+    }
+
+    @Override
+    public void registerDataAccount(String seed) {
+        CryptoAlgorithm algorithm = Crypto.getAlgorithm("ed25519");
+        SignatureFunction signFunc = Crypto.getSignatureFunction(algorithm);
+        AsymmetricKeypair cryptoKeyPair = signFunc.generateKeypair(seed.getBytes());
+        BlockchainKeypair keypair = new BlockchainKeypair(cryptoKeyPair.getPubKey(), cryptoKeyPair.getPrivKey());
+        eventContext.getLedger().dataAccounts().register(keypair.getIdentity());
+    }
+
+    @Override
+    public void registerEventAccount(String seed) {
+        CryptoAlgorithm algorithm = Crypto.getAlgorithm("ed25519");
+        SignatureFunction signFunc = Crypto.getSignatureFunction(algorithm);
+        AsymmetricKeypair cryptoKeyPair = signFunc.generateKeypair(seed.getBytes());
+        BlockchainKeypair keypair = new BlockchainKeypair(cryptoKeyPair.getPubKey(), cryptoKeyPair.getPrivKey());
+        eventContext.getLedger().eventAccounts().register(keypair.getIdentity());
+    }
+
+    @Override
+    public void publishEvent(String address, String topic, String content, long sequence) {
+        eventContext.getLedger().eventAccount(Bytes.fromBase58(address)).publish(topic, content, sequence);
     }
 
     @Override
